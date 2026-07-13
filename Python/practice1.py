e45e6280550ad1886964f44231c65fa54b196959
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-13 14:07:36 +0500

    Add files via upload

---
+++ Python/practice1.py
@@ -0,0 +1,32 @@
+# Practice
+# Q1
+first_name = "Yamin"
+age = 24
+height_cm = 6.2
+print(first_name,age,height_cm)
+
+# Q2
+city = "Faislabad"
+country = "Pakistan"
+print(f"{first_name} live in Faislabad, Pakistan")
+print(city.upper())
+
+# Q3
+a = input("X:")
+b = input("Y:")
+print(a + b)
+
+# Q4
+x = 10
+y = 2.5
+result = x + y
+print(type(result))
+
+# Q5
+num = 5
+res1 = num + 2.0
+res2 = str(num) + "3"
+print(res1)
+print(type(res1))
+print(res2)
+print(type(res2))
