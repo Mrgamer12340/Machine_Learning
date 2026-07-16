22867c70b40e860afd8a5e269f5232b434c1fe11
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-16 15:35:38 +0500

    Add files via upload

---
+++ Python/next.py
@@ -0,0 +1,28 @@
+name = "yamin"
+#is ka matlab hai ka o say 3 tak go be number yeh words aty hain wo print hoga
+print(name[0:3])     
+print(name[0:2])
+print(name.upper())
+print(name.isspace())
+print(name.lower())
+
+#user input
+# name = input("Please Enter You Name:") #user say kuch be data lena ka lia
+# print(name)
+num = input("Please Enter any number")
+#is me int is lia add kia hai q ka is ki datatype change krni hai
+print(int(num) + 6)
+l1 = "apple"
+l2 = "banana"
+l3 = "mango"
+print((l1,l2,l3) , "shake")
+#list method
+print(type(l1))
+l1 =[248373,248375,248353,244373]
+print(l1)
+l1.remove(248373)
+print(l1.count(248373))
+l1.sort()
+l1[0] ="yamin"
+print(l1)
+#tuples and method
