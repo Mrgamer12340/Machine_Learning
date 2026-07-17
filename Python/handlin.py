6f03df3691da8cd36eca610f316adc33894f6c6b
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-17 15:42:41 +0500

    Add files via upload

---
+++ Python/handlin.py
@@ -0,0 +1,18 @@
+# error handling is me zayada tak ye show hota
+# hai jub me hum koi fazool kam krta hain to error
+# ata hai is kam say apni website ko bachna ka lia
+# hum error handling ka use krta hain agar na use
+# to humari website or app slow par sakti hai
+
+try:
+    # kuch bhi kam krna hai
+    # aur zyada exception hai to humari error handling me catch krna hain
+    a = int(input("Enter your roll number: "))
+    print(2232 + a)
+    # humne kuch bhi error nahi aya to humari program me error message print kra hai
+
+except:
+        print("Error occurred while rolling")
+
+
+        
