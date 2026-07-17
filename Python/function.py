a3f67fb52de218be5afaf48f5f57fd6143e3d845
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-17 15:43:20 +0500

    Add files via upload

---
+++ Python/function.py
@@ -0,0 +1,28 @@
+
+# function me 2 items hota hain defination/ means wo kam kesa kra ga interlly
+# uses
+# Basic function syntax
+# Q1
+# def check_number():
+#     n = int(input("Please Enter the number: "))
+#     if n % 2 == 0:
+#         print(f"{n} , is even number")
+#     else:
+#         print(f"{n} , is odd number")
+
+
+# check_number()
+
+# Q2
+# def average(numbers):
+#     if len(numbers) == 0:
+#         return 0
+#     return sum(numbers) / len(numbers)
+# nums = [10, 20, 30, 40, 50]
+# print("Average:", average(nums))
+
+
+# Q3
+
+def number():
+    n = [1, 2, 3], [2, 5, 2], [3, 9, 8]
