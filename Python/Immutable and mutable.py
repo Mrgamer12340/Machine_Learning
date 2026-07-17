6480ab46160505682148fd0d24ca8686aef78ed3
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-17 15:42:07 +0500

    Add files via upload

---
+++ Python/Immutable and mutable.py
@@ -0,0 +1,4 @@
+# mutable datatype jo change ho sakta hain
+# list , set , dictionary bytearray array
+# Immutable datatype jo change nhi sakta hain
+# interger , floating ,boolean , string , truple , frozen set bytes
