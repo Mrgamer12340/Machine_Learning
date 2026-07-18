686924c36828fff8b3345c94863d1c0f59f40324
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-18 13:11:24 +0500

    Add files via upload

---
+++ Python/dictionary.py
@@ -0,0 +1,13 @@
+#Dictionary method
+a ={}
+print(a)
+print(type(a))
+dict1 ={"yamin" : 12 ,"ali" :33, "raza" :21}
+print(dict1["yamin"])
+#dictionary me hum key ki help say value find kr sakta hain like (yamin)
+#key and (12) is value 
+#dictionay ko hum change kr sakta hain
+dict1["haider"] = 33
+print(dict1)
+dict1["yamin"] =99
+print(dict1)
