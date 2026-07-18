db298643770c098a0eb291229351dc5b214b7bc2
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-18 13:11:40 +0500

    Add files via upload

---
+++ Python/app.py
@@ -0,0 +1,70 @@
+print("Helllo word")
+print("YAmin") 
+
+# topic #1
+# variable in python
+# python me data store krna ka lia hum variable ka use krta hain
+# variable number say start nahi ho sakta or is may space in ho sakti 
+# example  (a) is a container jis me kuch be store kr sakta hain
+a = 3
+b = 4
+print(a + b)
+a = 2
+print(a)
+a = "yamin"
+print(a)
+
+g = "y"
+y = "Gc university"
+
+print(g)
+
+#Topic #2
+#typecasting
+# typecasting means ek datatype ko dusri datatype me convert krna
+y = "10" #string / int
+print(int(y))
+h = 19   #number
+print(h)
+
+#Topic #3
+#opertors
+#1. Airthmetic operators
+num1 = 20
+num2 = 14
+print("num1 + Num2 is " ,num1 + num2)
+print("num1 - Num2 is " ,num1 - num2)
+print("num1 * Num2 is " ,num1 * num2)
+print("num1 / Num2 is " ,num1 / num2)
+#2. Assignment operators
+#value assign ko assignment opertor bolta hain like (yamin = 20)
+#yamin ko 20 number assign hova hai
+y = 20
+#example agr mujhe same value me kuch or add krna hai to me equal 
+#sign say pehla ko airthmetic operator add kru ga or sath new value
+y +=10
+print(y)
+#3. Comparsion operator
+#name is he pata chal raha hai yeh value ko compaire krta hain
+#is ka answer zayada tak true or false me ata hai
+y = 20
+a = 32
+m = 44
+print(y<a)
+print(y>a)
+print(y==a)
+print(y!=a)
+#4. Logical operator
+print(y==20 and m>a)
+#logical operator me asa hai ye to dono condition ture and 
+#ye dono me say ek agar dono me say ek ture hai or dusri false 
+#banti hai to logic false 
+print(y==20 or y>a) 
+#hum is ko reverse be kr sakta hain
+print(not(False))
+#not ka use kr ka hum true ko false or false or true bana sakta hain
+
+#Topic 4
+#Modules & pip
+#pip ki help say hum modules instrall kr sakta hain
+# string ko hum change nhi kr sakta hain
