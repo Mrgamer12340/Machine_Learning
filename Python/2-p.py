7c68c783dfb71d4af6baf40544030ac5bcf96745
Mr_Yameen <153727744+Mrgamer12340@users.noreply.github.com>
2026-07-20 23:24:00 +0500

    Add files via upload

---
+++ Python/2-p.py
@@ -0,0 +1,21 @@
+
+# a = int(input("Enter the number you find the square:"))
+
+# print("The is average of these given numbers", (a**2))
+
+
+
+# a = input("Please enter Your Good name:")
+# print(f"Good Morning {a}")
+
+
+
+a = input("Please enter Your Good name:")
+b = int(input("Please enter date:"))
+letter = (f""" Dear {a} ya
+       Your are selected!
+       {b}""")
+
+
+letter = ("Dear \n\tYamin you are a future ai developer")
+print(letter)
